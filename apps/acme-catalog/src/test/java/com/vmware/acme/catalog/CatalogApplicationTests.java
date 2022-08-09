74e87d061a6282f9a5d359e769911370dc530e63
Eddú Meléndez <eddu.melendez@gmail.com>
2022-08-09 15:25:22 -0500

    Add e2e test

--- apps/acme-catalog/src/test/java/com/vmware/acme/catalog/CatalogApplicationTests.java
+++ apps/acme-catalog/src/test/java/com/vmware/acme/catalog/CatalogApplicationTests.java
@@ -1,13 +1,58 @@
 package com.vmware.acme.catalog;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class CatalogApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int serverPort;
+
+    @Container
+    private static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:14.4-alpine3.16");
+
+    @DynamicPropertySource
+    static void sqlserverProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @BeforeEach
+    void before() {
+        RestAssured.port = serverPort;
+    }
+
+    @Test
+    void listAllProducts() {
+        given()
+                .get("/products")
+                .then()
+                .assertThat()
+                .body("data.size()", equalTo(8));
+    }
+
+    @Test
+    void findProductById() {
+        given()
+                .get("/products/533445d-530e-4a76-9398-5d16713b827b")
+                .then()
+                .assertThat()
+                .body("data.description", equalTo("Magic Yoga Mat!"));
+    }
 
 }
