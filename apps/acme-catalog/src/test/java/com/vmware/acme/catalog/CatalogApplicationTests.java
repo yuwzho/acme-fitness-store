143ecb167bbcaf50cdc94bfeabb2a51edf04dbcb
Eddú Meléndez <eddu.melendez@gmail.com>
2022-08-10 11:51:19 -0500

    Add prometheus sample

--- apps/acme-catalog/src/test/java/com/vmware/acme/catalog/CatalogApplicationTests.java
+++ apps/acme-catalog/src/test/java/com/vmware/acme/catalog/CatalogApplicationTests.java
@@ -1,21 +1,32 @@
 package com.vmware.acme.catalog;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.actuate.metrics.AutoConfigureMetrics;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
+import org.testcontainers.images.builder.Transferable;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.org.awaitility.Awaitility;
+
+import java.time.Duration;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {"management.endpoints.web.exposure.include=*",
+                "management.metrics.export.prometheus.step=2s"})
+@AutoConfigureMetrics
 @Testcontainers
 class CatalogApplicationTests {
 
@@ -34,25 +45,66 @@ class CatalogApplicationTests {
 
     @BeforeEach
     void before() {
-        RestAssured.port = serverPort;
+        org.testcontainers.Testcontainers.exposeHostPorts(this.serverPort);
+        RestAssured.port = this.serverPort;
     }
 
     @Test
     void listAllProducts() {
-        given()
-                .get("/products")
-                .then()
-                .assertThat()
-                .body("data.size()", equalTo(8));
+        try (final GenericContainer prometheus = createPrometheus()) {
+            prometheus.start();
+            given()
+                    .get("/products")
+                    .then()
+                    .assertThat()
+                    .body("data.size()", equalTo(8));
+            checkMetric(prometheus, "/products");
+        }
     }
 
     @Test
     void findProductById() {
-        given()
-                .get("/products/533445d-530e-4a76-9398-5d16713b827b")
-                .then()
-                .assertThat()
-                .body("data.description", equalTo("Magic Yoga Mat!"));
+        try (final GenericContainer prometheus = createPrometheus()) {
+            prometheus.start();
+            given()
+                    .get("/products/533445d-530e-4a76-9398-5d16713b827b")
+                    .then()
+                    .assertThat()
+                    .body("data.description", equalTo("Magic Yoga Mat!"));
+            checkMetric(prometheus, "/products/{id}");
+        }
+    }
+
+    private GenericContainer createPrometheus() {
+        var config = String.format("scrape_configs:\n" +
+                "  - job_name: \"prometheus\"\n" +
+                "    scrape_interval: 2s\n" +
+                "    metrics_path: \"/actuator/prometheus\"\n" +
+                "    static_configs:\n" +
+                "      - targets: ['host.testcontainers.internal:%s']", this.serverPort);
+        return new GenericContainer<>("prom/prometheus:v2.37.0")
+                .withExposedPorts(9090)
+                .waitingFor(new LogMessageWaitStrategy().withRegEx("(?s).*Server is ready to receive web requests.*$"))
+                .withAccessToHost(true)
+                .withCopyToContainer(Transferable.of(config), "/etc/prometheus/prometheus.yml");
+    }
+
+    private void checkMetric(GenericContainer prometheus, String path) {
+        var query = String.format("store_products_seconds_count{uri=\"%s\"}", path);
+        Awaitility.given().pollInterval(Duration.ofSeconds(2))
+                .atMost(Duration.ofSeconds(15))
+                .ignoreExceptions()
+                .untilAsserted(() -> {
+                    given().baseUri("http://" + prometheus.getHost())
+                            .port(prometheus.getMappedPort(9090))
+                            .queryParams(Map.of("query", query))
+                            .get("/api/v1/query")
+                            .prettyPeek()
+                            .then()
+                            .assertThat()
+                            .statusCode(200)
+                            .body("data.result[0].value", hasItem("1"));
+                });
     }
 
 }
