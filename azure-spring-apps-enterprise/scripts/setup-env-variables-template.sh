05c415c2fadeb960a02d39965d307c6e8a010dda
ericm48 <me_manley@yahoo.com>
2023-08-02 10:18:15 -0600

    -Scripts folder take1

--- azure-spring-apps-enterprise/scripts/setup-env-variables-template.sh
+++ azure-spring-apps-enterprise/scripts/setup-env-variables-template.sh
@@ -1,8 +1,8 @@
-export SUBSCRIPTION=CHANGE-ME                 # replace it with your subscription-id
-export RESOURCE_GROUP=acme-fitness-rg           # existing resource group or one that will be created in next steps
-export SPRING_APPS_SERVICE=acme-fitness-YOUR-UNIQUE-USERNAME   # A unique name of the service that will be created in the next steps
-export LOG_ANALYTICS_WORKSPACE=acme-fitness-la-CHANGE-ME   # existing workspace or one that will be created in next steps
-export REGION=canadacentral                          # choose a region with Enterprise tier support
+export SUBSCRIPTION=subscription-id                 # replace it with your subscription-id
+export RESOURCE_GROUP=resource-group-name           # existing resource group or one that will be created in next steps
+export SPRING_APPS_SERVICE=azure-spring-apps-name   # name of the service that will be created in the next steps
+export LOG_ANALYTICS_WORKSPACE=log-analytics-name   # existing workspace or one that will be created in next steps
+export REGION=region-name                           # choose a region with Enterprise tier support
 
 export CART_SERVICE_APP="cart-service"
 export IDENTITY_SERVICE_APP="identity-service"
@@ -11,3 +11,4 @@ export PAYMENT_SERVICE_APP="payment-service"
 export CATALOG_SERVICE_APP="catalog-service"
 export FRONTEND_APP="frontend"
 
+export CUSTOM_BUILDER="no-bindings-builder"
