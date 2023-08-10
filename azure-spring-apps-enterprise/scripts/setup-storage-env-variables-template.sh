261fae12c3981588a7a31602f31b9eb99d0ebc46
ericm48 <me_manley@yahoo.com>
2023-08-10 09:26:36 -0600

    -Added storage script template, instructions.

--- azure-spring-apps-enterprise/scripts/setup-storage-env-variables-template.sh
+++ azure-spring-apps-enterprise/scripts/setup-storage-env-variables-template.sh
@@ -1,2 +1,5 @@
 export STORAGE_RESOURCE_GROUP='change-me'         # different resource group from previous steps
 export STORAGE_ACCOUNT_NAME='change-me'           # choose a name for your storage account
+
+echo $STORAGE_RESOURCE_GROUP
+echo $STORAGE_ACCOUNT_NAME
