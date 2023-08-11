7d3fe4afa5df4653939be2a2a81c4d4aecb77db1
Dingmeng Xue <dixue@microsoft.com>
2023-08-11 22:49:45 +0800

    Move python file to subfolder (#21)

--- apps/acme-assist/prepare_data.sh
+++ apps/acme-assist/prepare_data.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 INITIAL_WORKING_DIRECTORY=$(pwd)
 cd "$(dirname "$0")"
-python ./generate_db_migration.py > ./V1_1__insert_products.sql
+python ./data/generate_db_migration.py > ./V1_1__insert_products.sql
 mv ./*.sql ../acme-catalog/src/main/resources/db/migration
 cp ./data/images/* ../acme-catalog/src/main/resources/static/
 cd $INITIAL_WORKING_DIRECTORY
