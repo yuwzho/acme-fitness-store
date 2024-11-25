dbe71345a8343ea53fd800b8359c0f6e30940829
yuwzho <yuwzho@microsoft.com>
2024-11-25 11:35:35 +0800

    Add version

--- azure-kubernetes-service/resources/var.sh
+++ azure-kubernetes-service/resources/var.sh
@@ -12,14 +12,17 @@ POSTGRESQL_NAME="${RESOURCE_GROUP}-postgres"
 REDIS_NAME="${RESOURCE_GROUP}-redis"
 
 # Docker image tags for Spring Cloud components
-EUREKA_IMAGE_TAG="acrbuild-eureka"
-CONFIGSERVER_IMAGE_TAG="acrbuild-config-server"
-SPRING_BOOT_ADMIN_IMAGE_TAG="acrbuild-spring-boot-admin"
-GATEWAY_IMAGE_TAG="acrbuild-spring-cloud-gateway"
+EUREKA_IMAGE_TAG="acrbuild-eureka-0.0.1-SNAPSHOT"
+CONFIGSERVER_IMAGE_TAG="acrbuild-config-server-0.0.1-SNAPSHOT"
+SPRING_BOOT_ADMIN_IMAGE_TAG="acrbuild-spring-boot-admin-0.0.1-SNAPSHOT"
+GATEWAY_IMAGE_TAG="acrbuild-spring-cloud-gateway-0.0.1-SNAPSHOT"
+
+# Update docker image tag
+IMAGE_VERSION="0.0.1-SNAPSHOT"
 
 # Docker image tags for applications
-CATALOG_SERVICE_APP_IMAGE_TAG="buildpack-catalog"
-PAYMENT_SERVICE_APP_IMAGE_TAG="buildpack-payment"
-ORDER_SERVICE_APP_IMAGE_TAG="buildpack-order"
-CART_SERVICE_APP_IMAGE_TAG="buildpack-cart"
-FRONTEND_APP_IMAGE_TAG="buildpack-frontend"
+CATALOG_SERVICE_APP_IMAGE_TAG="buildpack-catalog-${IMAGE_VERSION}"
+PAYMENT_SERVICE_APP_IMAGE_TAG="buildpack-payment-${IMAGE_VERSION}"
+ORDER_SERVICE_APP_IMAGE_TAG="buildpack-order-${IMAGE_VERSION}"
+CART_SERVICE_APP_IMAGE_TAG="buildpack-cart-${IMAGE_VERSION}"
+FRONTEND_APP_IMAGE_TAG="buildpack-frontend-${IMAGE_VERSION}"
